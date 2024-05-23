ce69f11c47fa783f424b8e23381119d358739f41
Gabriel Oliveira <gabriel_lopeslomeu@hotmail.com>
2024-05-23 20:52:16 -0300

    edit: sentence from whatsappSend

--- src/services/whatsappSend.js
+++ src/services/whatsappSend.js
@@ -10,7 +10,7 @@ const response = await fetch(GZAPPY_URL, {
   body: JSON.stringify({
     instance_id: 'RLJV1MCXSFOGK9K7NEQ03KKH',
     instance_token: 'f79bd023-dc65-444c-ba09-fbab274b6570',
-    message: [`Pode deixar que anotei aqui o presente! Obrigado ${name}`, `- 1 ${item}: ${description}`],
+    message: [`Pode deixar que anotei aqui o presente! Obrigado ${name}.`, `- 1 ${item}: ${description}`,`Se tiver qualquer dúvida é só mandar mensagem.`],
     phone: [`55${cell}`],
     mediaUrl: "https://uploaddeimagens.com.br/images/004/726/327/full/anuncio_GZAPPY_IMAGENS.jpg?1706576404"
   })
